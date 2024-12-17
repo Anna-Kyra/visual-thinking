38e3ce0fb5b1fd6bce673758d6526291b581d49f
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2024-12-17 21:01:33 +0100

    Icy background overlay
    Wanneer de cursor niet beweegt voor 5 seconden krijg je het koud en moet je bewegen om het ijs te doorbreken

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,47 @@
 <script>
     import Header from "$lib/molecules/header.svelte";
     import Footer from "$lib/molecules/footer.svelte";
+
+    import { onMount } from "svelte";
+
+    let box // .icy-overlay
+    let timeoutId
+    let isInactive = false
+    let opacityFreeze = 0
+
+    function toggleOverlay(inactive) {
+        isInactive = inactive;
+        if (inactive) {
+            console.log('freeze')
+            opacityFreeze = 100
+        } else {
+            console.log('unfreeze')
+            opacityFreeze = 0
+            
+        }
+    }
+
+    function resetTimer() {
+        if (isInactive) {
+            toggleOverlay(false)
+        }
+
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => toggleOverlay(true), 4000)
+    }
+
+    onMount(() => {
+        window.addEventListener("mousemove", resetTimer)
+
+        resetTimer()
+
+        return () => {
+            window.removeEventListener("mousemove", resetTimer);
+            clearTimeout(timeoutId);
+        }
+    })
+
+    
     
 </script>
 
@@ -10,3 +51,24 @@
 
 <Footer />
 
+<div bind:this={box} class="icy-overlay" style="--opacity-freeze: {opacityFreeze}"></div>
+
+<style>
+    .icy-overlay {
+        position: fixed;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(31, 31, 138, 0.105);
+        background-image: url('/images/icy.png');
+        background-size: cover;
+        /* mix-blend-mode: multiply; */
+        --opacity-freeze: 100%;
+        opacity: calc(var(--opacity-freeze) * 1%);
+        z-index: 9;
+        pointer-events: none;
+        transition: opacity 5s cubic-bezier(0.165, 0.84, 0.44, 1);
+
+    }
+
+</style>
