f0a57754b399228eca2ae78082a41cebf0fded6d
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2024-12-20 11:41:34 +0100

    christmas bell bug

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -2,7 +2,7 @@ console.log('werkt!!')
 
 let soundBell = new Audio('/video/bell_sound.mp3')
 
-let links = document.querySelectorAll("a")
+let links = document.querySelectorAll("a, span")
 links.forEach(function(link) {
     link.addEventListener("click", function() {
         // console.log('Link geklikt')
