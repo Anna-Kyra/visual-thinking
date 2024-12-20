57cd82f01e1b7ef487f384a170081cf7eb388c96
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2024-12-20 11:11:53 +0100

    Bell sound links

---
+++ static/scripts/main.js
@@ -0,0 +1,11 @@
+console.log('werkt!!')
+
+let soundBell = new Audio('/video/bell_sound.mp3')
+
+let links = document.querySelectorAll("a")
+links.forEach(function(link) {
+    link.addEventListener("click", function() {
+        // console.log('Link geklikt')
+        soundBell.play()
+    })
+})
